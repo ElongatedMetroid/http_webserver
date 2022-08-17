7980e9e770cf5f8ee04ac92af4c0abe920b30c8a
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-08-17 09:39:29 -0700

    Listen to TCP connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{net::TcpListener, process};
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new TcpListener
+    let listener = 
+        // Returns a new TcpListener instance that will be bound to the port 7878
+        match TcpListener::bind("127.0.0.1:7878") {
+            Ok(v) => v,
+            Err(e) => {
+                println!("Port could not be bound: {e}");
+                process::exit(1);
+            }
+        };
+
+    // Iterate through each each stream between the client and the server, this
+    // could also be seen as iterating between each connection attempt.
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
