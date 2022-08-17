64db344b24c9bae57ce8bd457d5d759067b3d966
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-08-17 11:03:29 -0700

    add 404 error page

--- src/main.rs
+++ src/main.rs
@@ -29,30 +29,35 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     // Create a new BufRead instance that wraps a mutable reference to the stream
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        // An iterator that splits the stream of data whenever it sees a newline
-        // byte. Returns a result containing a String
-        .lines()
-        // Unwrap each map to get each String contained
-        .map(|result| result.unwrap())
-        // Take lines until we get a line that is an empty String, since the 
-        // browser signals the end of an HTTP request by sending two newline
-        // character in a row.
-        .take_while(|line| !line.is_empty())
-        // Collect the lines into a vector
-        .collect();
+    // Read the first line of the HTTP request
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // Status line part of a response that uses HTTP version 1.1, has a status 
-    // code of 200, and an OK reason phrase, no headers, and no body
-    let status_line = "HTTP/1.1 200 OK";
-    // Read the HTML document into a String
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+    // Check if the request line to see if it is a GET request to the path /,
+    // if it is we will return the contents of our HTML file.
+    if request_line == "GET / HTTP/1.1" {
+        // Status line part of a response that uses HTTP version 1.1, has a status 
+        // code of 200, and an OK reason phrase, no headers, and no body
+        let status_line = "HTTP/1.1 200 OK";
+        // Read the HTML document into a String
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
 
-    let response = 
         // Format the files contents as the body of the success response
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+    
+        // Send the response data as bytes directly down the connection.
+        stream.write_all(response.as_bytes()).unwrap();
+    } else { 
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
 
-    // Send the response data as bytes directly down the connection.
-    stream.write_all(response.as_bytes()).unwrap();
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
