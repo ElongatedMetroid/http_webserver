5489b2e0d2503fe219bfc54e1f9b10ca5500a640
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-08-17 16:38:07 -0700

    add sender field to ThreadPool and send recivers to each Worker using an Arc pointer

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
-use std::{thread::{Builder, JoinHandle}, process};
-
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-}
+use std::{
+    process,
+    thread::{Builder, JoinHandle}, 
+    sync::{mpsc, Arc, Mutex},
+};
 
 #[derive(Debug)]
 pub enum PoolCreationError {
     ZeroThreads(&'static str)
 }
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+struct Job;
 
 impl ThreadPool {
     /// Creates a new ThreadPool.
@@ -26,6 +33,10 @@ impl ThreadPool {
             ) 
         }
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         // Create the vector of threads, with a preallocated size
         let mut workers = Vec::with_capacity(size);
 
@@ -34,10 +45,10 @@ impl ThreadPool {
             // thread::spawn cannot be used here since it expects to get some 
             // code that the thread should run immediately. But we just want to
             // create threads and have them wait until we create code later.
-            workers.push(Worker::new(i));
+            workers.push(Worker::new(i, Arc::clone(&receiver)));
         }
         
-        Ok(ThreadPool { workers })
+        Ok(ThreadPool { workers, sender })
     }
 
     pub fn execute<F>(&self, f: F)
@@ -60,9 +71,9 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
+    fn new(id: usize, recieiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let builder = Builder::new();
-        let thread = match builder.spawn(|| {}) {
+        let thread = match builder.spawn(|| { recieiver; }) {
             Ok(t) => t,
             Err(e) => {
                 println!("Failed to create worker {}: {}", id, e);
