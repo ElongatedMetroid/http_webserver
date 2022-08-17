9dfd243e9bc84414b98e76c2cc6a93f92e35365c
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-08-17 09:58:50 -0700

    Read and print browser request

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{net::TcpListener, process};
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    process,
+};
 
 fn main() {
     // Create a new TcpListener
@@ -17,6 +21,26 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+/// Read data from the TCP stream and print it.
+fn handle_connection(mut stream: TcpStream) {
+    // Create a new BufRead instance that wraps a mutable reference to the stream
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        // An iterator that splits the stream of data whenever it sees a newline
+        // byte. Returns a result containing a String
+        .lines()
+        // Unwrap each map to get each String contained
+        .map(|result| result.unwrap())
+        // Take lines until we get a line that is an empty String, since the 
+        // browser signals the end of an HTTP request by sending two newline
+        // character in a row.
+        .take_while(|line| !line.is_empty())
+        // Collect the lines into a vector
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
